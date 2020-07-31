5630d1f1bd19e0a05de71acc4afed9b91155ddda
jumang4423 <catontheskate4423@gmail.com>
2020-07-31 18:15:25 +0900

    Final revision

--- main.cpp
+++ main.cpp
@@ -28,11 +28,11 @@ int main()
     std::cout << "Heads: " << heads << ", Tails: " << tails << std::endl;
     if(tails<heads)
     {
-        std::cout << "You won!" << std::endl;
+        std::cout << name << " won!" << std::endl;
     }
     else
     {
-        std::cout << "You lost." << std::endl;
+        std::cout << name << " lost." << std::endl;
     }
     
     return 0;
