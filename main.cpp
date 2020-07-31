68ad35cc99926d826b46d385517446d13498457f
jumang4423 <catontheskate4423@gmail.com>
2020-07-31 17:56:31 +0900

    HoT: Initial revision

--- main.cpp
+++ main.cpp
@@ -1,4 +1,27 @@
+#include <bits/stdc++.h>
+
 int main()
 {
+    std::vector<bool> data(3);
+    double stat;
+    int heads = 0, tails = 0;
+    std::cout << "Tossing a coin..." << std::endl;
+    for(int i=0; i<3; i++) 
+    {
+        std::cout << "Round " << (i+1) << " ";
+        stat = (double)rand()/RAND_MAX;
+        if(stat<0.5) 
+        {
+            std::cout << "Heads" << std::endl;
+            heads++;
+        }
+        else 
+        {
+            std::cout << "Tails" << std::endl;
+            tails++;
+        }
+    }
+    std::cout << "Heads: " << heads << ", Tails: " << tails << std::endl;
+
     return 0;
 }
